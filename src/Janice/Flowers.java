cb4c17516647de9d2e7cb42054f3da123606599c
GayanganJanice <Janicegayangan@gmail.com>
2023-11-16 16:36:39 +0800

    defined classes added method, constructor and data field

--- src/Janice/Flowers.java
+++ src/Janice/Flowers.java
@@ -1,10 +1,21 @@
 
 package Janice;
 
-/**
- *
- * @author Admin
- */
+
 public class Flowers {
+    String flower_name;
+    String flower_quantity;
     
+    Flowers(String flower_name, String flower_quantity){
+     this.flower_name=flower_name;
+     this.flower_quantity = flower_quantity;
+    }
+    
+   void flower_Name(){
+       System.out.println(flower_name+" nice");
+   }
+   
+   void flower_Quantity(){
+       System.out.println(flower_name+" daghan kaayo");
+   }
 }
