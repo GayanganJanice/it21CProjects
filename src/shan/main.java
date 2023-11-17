26c5e3d07ebe976f501e008c2cccc82ce9d83418
GayanganJanice <janicegayangan@gmail.com>
2023-11-17 14:13:44 +0800

    added shan class

--- src/shan/main.java
+++ src/shan/main.java
@@ -1,14 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package shan;
 
-/**
- *
- * @author Admin
- */
+
 public class main {
+     public static void main(String[]args){
+        System.out.println("Iam Janice Gayangan");
+        System.out.println("Live in San Roque Sumilao Bukidnon");
+        System.out.println("19 yrs old");
+        
+    }
+    
     
 }
+
+    
+
